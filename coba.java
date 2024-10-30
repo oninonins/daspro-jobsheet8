456d4902af9cab604457dba745d76a67f4af0d3f
oninonins <varelinobubadibako11@gmail.com>
2024-10-30 11:09:03 +0700

    new

---
+++ coba.java
@@ -0,0 +1,12 @@
+public class coba {
+    public static void main(String[] args) {
+        
+
+        for (int i=0; 5 >= i; i++) {
+            for (int j = 0; j <= 5; j++)
+                System.out.println(" *");
+            
+            System.out.println();
+        }
+    }
+}
